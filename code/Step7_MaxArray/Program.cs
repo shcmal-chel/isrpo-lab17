5eafaee7f9ebcef9c45357fafa5a62033570332e
shcmal-chel <anastasia9cheshuina@yandex.ru>
2026-03-11 09:49:53 +0300

    Step 7 arrays

---
+++ code/Step7_MaxArray/Program.cs
@@ -0,0 +1,12 @@
+﻿System.Console.WriteLine("Дан массив: { 5, 4, 5, 3, 4 }");
+int[] numbers = { 5, 4, 5, 3, 4 };
+int max = numbers[0];
+
+for (int i = 1; i < numbers.Length; i++)
+{
+    if (numbers[i] > max)
+    {
+        max = numbers[i];
+    }
+}
+Console.WriteLine($"Максимальный элемент: {max}");
