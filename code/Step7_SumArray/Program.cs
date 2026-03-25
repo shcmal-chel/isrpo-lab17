ecbac99b8d5e8b97f93fbf8ffc2947200f0392a8
shcmal-chel <anastasia9cheshuina@yandex.ru>
2026-03-25 09:35:47 +0300

    Added array

---
+++ code/Step7_SumArray/Program.cs
@@ -0,0 +1,9 @@
+﻿System.Console.WriteLine("Дан массив: { 5, 4, 5, 3, 4 }");
+int[] numbers = { 5, 4, 5, 3, 4 };
+int sum = 0;
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    sum = sum + numbers[i];
+}
+Console.WriteLine($"Сумма элементов: {sum}");
